a8c0dde2eb0b7c58802a8fbe70b776a7e9087d5a
zhtcks <zhtcks@gmail.com>
2022-05-09 21:48:11 +0900

    add: data.sql 추가(categories 테이블 초기화)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories(id, name, description) VALUES (null, '문화/예술/공연', '문화/예술/공연 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '봉사/사회활동', '봉사/사회활동 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '학술/교양', '학술/교양 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '창업/취업', '창업/취업 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '어학', '어학 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '체육', '체육 관련 동아리입니다.');
+INSERT INTO categories(id, name, description) VALUES (null, '친목', '친목 관련 동아리입니다.');

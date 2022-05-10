715a589de5b8942203de745174b134e391cf1287
zhtcks <zhtcks@gmail.com>
2022-05-11 02:55:24 +0900

    add: https 설정 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ then
 fi
 
 echo "start docker-compose down: ubuntu"
-sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose-dev.yml down
+sudo docker-compose -f /home/ubuntu/docker-compose-dev.yml down
 
 echo "start docker-compose up: ubuntu"
-sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose-dev.yml up --build -d
+sudo docker-compose -f /home/ubuntu/docker-compose-dev.yml up --build -d
